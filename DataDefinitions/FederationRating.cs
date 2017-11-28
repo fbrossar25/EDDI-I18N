643a6ebb3e85409c80f4a673e3f78e23cc345861
StivlWrith <32070483+StivlWrith@users.noreply.github.com>
2017-11-28 21:19:07 +0100

    Update Class FromName(string from)
    Add a test to find with v.LocalName if v.name return null

--- DataDefinitions/FederationRating.cs
+++ DataDefinitions/FederationRating.cs
@@ -58,6 +58,11 @@ namespace EddiDataDefinitions
             }
 
             FederationRating result = RATINGS.FirstOrDefault(v => v.name == from);
+            // test LocalName
+            if (result == null)
+            {
+                result = RATINGS.FirstOrDefault(v => v.LocalName == from);
+            }
             if (result == null)
             {
                 Logging.Report("Unknown Federation Rating name " + from);
