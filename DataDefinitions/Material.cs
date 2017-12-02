80f163ba4b018f2b44186d018a05a8623c50ebe3
StivlWrith <32070483+StivlWrith@users.noreply.github.com>
2017-12-02 23:37:47 +0100

    Add LocalLocation
    Localisation of the location describe in http://api.eddp.co/_materiallocations

--- DataDefinitions/Material.cs
+++ DataDefinitions/Material.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using Utilities;
 using Newtonsoft.Json;
@@ -41,6 +41,15 @@ namespace EddiDataDefinitions
 
         // Location of the material
         public string location { get; set; }
+
+        public string LocalLocation
+        {
+            get
+            {
+                return I18N.GetString(location) ?? location;
+            }
+
+        }
 
         private Material(string EDName, string category, string name, Rarity rarity, string symbol = null, decimal? goodpctbody = null, decimal? greatpctbody = null)
         {
